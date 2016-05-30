c182b4675910e5a8eff44d1c2c6969f77552f5e6
Skip Rhudy <skiprhudy@hotmail.com>
2016-05-30 13:34:05 -0500

    refactor format_date into shared listable. this is a bad design decision imho

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,5 +3,32 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  # this could get ugly, and i don't think sharing 'format_date'
+  # between classes that have potentially very different
+  # date requirements is a good idea.
+  def format_date(options = {})
+    if options[:due]
+      due = options[:due]
+      return due ? due.strftime('%D') : 'No due date'
+    end
+
+    # completely different requirement than for todo class,
+    # so why is it shared?
+    start_date = options[:start_date] if options[:start_date]
+    dates = start_date.strftime('%D') if start_date
+    end_date = options[:end_date] if options[:end_date]
+    dates << ' -- ' + end_date.strftime('%D') if end_date
+    dates = 'N/A' if !dates
+    return dates
+  end
+
+  def format_priority
+    value = ' ⇧' if @priority == 'high'
+    value = ' ⇨' if @priority == 'medium'
+    value = ' ⇩' if @priority == 'low'
+    value = '' if !@priority
+    return value
+  end
 end
 
