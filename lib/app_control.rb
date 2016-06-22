2f9ec1df4bc7f369990f4a8c65951e221db2c82c
skiprhudy <skiprhudy@gmail.com>
2016-06-22 17:14:42 -0500

    implement delete item menu system

--- lib/app_control.rb
+++ lib/app_control.rb
@@ -28,6 +28,7 @@ class AppControl
       menu.choice('By Item Type') { list_by_type }
       menu.choice('Go back') { return }
     end
+    puts
   end
 
   def list_by_type
@@ -42,21 +43,14 @@ class AppControl
     puts
   end
 
-  # HighLine doesn't yet support adding items to menu programmatically (well, it
-  # does on 'master' branch, but master isn't what gets installed with gem install
-  # or bundle install, right now is limited to 1.7.8 ... no adding items til new master
-  # is released. so this is lame ... can update with new functionality in HighLine
-  # is released)
   def delete_items
     puts
-    @list.all
+    answer = @cli.choose do |menu|
+      @list.all.each_with_index do |item, idx |
+        menu.choice(item.description) { @list.delete(idx + 1)}
+      end
+      menu.prompt = 'Choose item to delete from above:'
+    end
     puts
-    puts "Enter item to delete:"
-    number = STDIN.getc
-    STDIN.flush
-    @list.delete(number.to_i)
-    puts
-    @list.all
   end
-
 end
